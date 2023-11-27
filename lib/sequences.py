2e0703485b72ade9469339015827030e7fb40434
DennohDev <dennismaina1024@gmail.com>
2023-11-27 12:51:33 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    if length == 0:
+        print(fibonacci_list)
+    elif length == 1:
+        fibonacci_list.append(0)
+        print(fibonacci_list)
+    elif length == 2:
+        fibonacci_list.append(0)
+        fibonacci_list.append(1)
+        print(fibonacci_list)
+    elif length >= 3:
+        fibonacci_list.append(0)
+        fibonacci_list.append(1)
+        for i in range(2, length):
+            number = fibonacci_list[i-1] + fibonacci_list[i-2]
+            fibonacci_list.append(number)
+            i+=1
+        print(fibonacci_list)
+    
